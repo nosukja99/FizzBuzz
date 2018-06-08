fd35794292bf2f524aeaa0f1ed5c61efb3e8c03d
nosukja99 <nosukja99@gmail.com>
2018-06-08 13:23:34 -0400

    Bonus version of FizzBuzz - I've got this!

--- src/com/company/Print.java
+++ src/com/company/Print.java
@@ -1,62 +1,137 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Print {
 
     int start=1;
     int end=0;
+    int count=0;
+    ArrayList<String> wordList = new ArrayList<>();
 
     public Print()
     {
+        wordList.add("Rock");
+        wordList.add("Paper");
+        wordList.add("Scissors");
+        wordList.add("Lizard");
+        wordList.add("Spock");
+    }
 
+    public String getWordFromList(int count)
+    {
+        int  index;
+        String word="";
+        if (count % 5 == 0) {
+            index = 0;
+            word=wordList.get(index);
+        }
+        else if (count % 5 == 1) {
+            index = 1;
+            word = wordList.get(index);
+        }
+        else if (count % 5 == 2) {
+            index = 2;
+            word = wordList.get(index);
+        }
+        else if (count % 5 == 3) {
+            index = 3;
+            word = wordList.get(index);
+        }
+        else
+        {
+            index = 4;
+            word = wordList.get(index);
+        }
+        return word;
     }
 
     public String makePrint ()
     {
 
-        while(start>end) {
-            Random rand = new Random();
-            start = 1 + rand.nextInt(100);
-            //System.out.println("starting number: " + start);
+            while(start>end) {
+                Random rand = new Random();
+                start = 1 + rand.nextInt(100);
+                //System.out.println("starting number: " + start);
 
-            Random rand1 = new Random();
-            end = 1 + rand1.nextInt(100);
-            //System.out.println("ending number: " + end);
-        }
+                Random rand1 = new Random();
+                end = 1 + rand1.nextInt(100);
+                //System.out.println("ending number: " + end);
+            }
 
-        String print = "";
+            String print = "";
+
             if (end-start<10)
             {
                 print = "This won't take long.\n\n";
             }
-            for (int i = start; i <= end; i++) {
 
-                if (start == end) {
-                    print = "Shazam!!";
-                }
-                else if (i % 5 == 0 && i % 3 != 0 ) {
-                    if(i % 7 != 0)
-                        print = print + "Buzz\n";
-                    else
-                        print = print + "Buzz - this number is a multiple of seven\n";
-                } else if (i % 3 == 0 && i % 5 != 0) {
-                    if(i % 7 != 0)
-                        print = print + "Fizz\n";
-                    else
-                        print = print + "Fizz - this number is a multiple of seven\n";
-
-                } else if (i % 15 == 0) {
-                    if(i % 7 != 0)
-                        print = print + "Fizz Buzz\n";
-                    else
-                        print = print + "Fizz Buzz - this number is a multiple of seven\n";
-                } else if (i%7 == 0)
+            for (int i = start; i <= end; i++)
+            {
+                if(i>10 && i%2==0)
                 {
-                    print = print + Integer.toString(i) +" - this number is a multiple of seven\n";
+                    if (start == end)
+                        print = "Shazam!!";
+                    else if (i % 5 == 0 && i % 3 != 0) {
+                        if (i % 7 != 0) {
+                            print = print + "Buzz - " + getWordFromList(count)+"\n";
+                            count++;
+                        }
+                        else {
+                            print = print + "Buzz - this number is a multiple of seven - " + getWordFromList(count) + "\n";
+                            count++;
+                        }
+                    } else if (i % 3 == 0 && i % 5 != 0) {
+                        if (i % 7 != 0) {
+                            print = print + "Fizz - " + getWordFromList(count) + "\n";
+                            count++;
+                        }
+                        else {
+                            print = print + "Fizz - this number is a multiple of seven - " + getWordFromList(count) + "\n";
+                            count++;
+                        }
+                    } else if (i % 15 == 0) {
+                        if (i % 7 != 0) {
+                            print = print + "Fizz Buzz - " + getWordFromList(count) + "\n";
+                            count++;
+                        }
+                        else {
+                            print = print + "Fizz Buzz - this number is a multiple of seven - " + getWordFromList(count) + "\n";
+                            count++;
+                        }
+                    } else if (i % 7 == 0) {
+                        print = print + Integer.toString(i) + " - this number is a multiple of seven - "+ getWordFromList(count)+"\n";
+                        count++;
+                    } else {
+                        print = print + Integer.toString(i) +" - "+ getWordFromList(count)+"\n";
+                        count++;
+                    }
                 }
-                else {
-                    print = print + Integer.toString(i) + "\n";
+                else
+                {
+                    if (start == end)
+                        print = "Shazam!!";
+                    else if (i % 5 == 0 && i % 3 != 0) {
+                        if (i % 7 != 0)
+                            print = print + "Buzz\n";
+                        else
+                            print = print + "Buzz - this number is a multiple of seven\n";
+                    } else if (i % 3 == 0 && i % 5 != 0) {
+                        if (i % 7 != 0)
+                            print = print + "Fizz\n";
+                        else
+                            print = print + "Fizz - this number is a multiple of seven\n";
+                    } else if (i % 15 == 0) {
+                        if (i % 7 != 0)
+                            print = print + "Fizz Buzz\n";
+                        else
+                            print = print + "Fizz Buzz - this number is a multiple of seven\n";
+                    } else if (i % 7 == 0) {
+                        print = print + Integer.toString(i) + " - this number is a multiple of seven\n";
+                    } else {
+                        print = print + Integer.toString(i) + "\n";
+                    }
                 }
             }
 
