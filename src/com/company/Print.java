aa0fa6b7e8018e628b4045f9c8fe2a48249cdf0a
nosukja99 <nosukja99@gmail.com>
2018-06-08 10:24:46 -0400

    Shazam version of FizzBuzz

--- src/com/company/Print.java
+++ src/com/company/Print.java
@@ -1,40 +1,48 @@
 package com.company;
 
+import java.util.Random;
+
 public class Print {
 
-    int max;
+    int start=2;
+    int end=1;
 
     public Print()
     {
 
     }
-    public Print(int n)
-    {
-       max = n;
-    }
 
     public String makePrint ()
     {
-        String print = "";
-        for (int i=1; i<=max; i++)
-        {
-            if (i%5==0&&i%3!=0)
-            {
-                print = print+"Buzz\n";
-            }
-            else if (i%3==0&&i%5!=0)
-            {
-                print = print+"Fizz\n";
-            }
-            else if (i%15==0)
-            {
-                print=print+"Fizz Buzz\n";
-            }
-            else
-            {
-                print=print+Integer.toString(i)+"\n";
-            }
+
+        while(start>end) {
+            Random rand = new Random();
+            start = 1 + rand.nextInt(100);
+            //System.out.println("starting number: " + start);
+
+            Random rand1 = new Random();
+            end = 1 + rand1.nextInt(100);
+            // System.out.println("ending number: " + end);
         }
+
+        String print = "";
+
+            for (int i = start; i <= end; i++) {
+
+                if (start == end) {
+                    print = "Shazam!!";
+                }
+                else if (i % 5 == 0 && i % 3 != 0) {
+                    print = print + "Buzz\n";
+                } else if (i % 3 == 0 && i % 5 != 0) {
+                    print = print + "Fizz\n";
+                } else if (i % 15 == 0) {
+                    print = print + "Fizz Buzz\n";
+                } else {
+                    print = print + Integer.toString(i) + "\n";
+                }
+            }
+
         return print;
     }
 }
